3b8b9dcb0c21ed8bbd79e1c85b83c4b6b34e9702
DevYeop <lee.sangyeop0715@gmail.com>
2024-10-11 14:12:19 +0900

    feat: 환경 변수를 코드에서 분리 및 JWT 프로세서 초기화 개선
    - JWT 프로세서 빈 생성 시 application.properties의 데이터가 정상적으로 주입되도록 수정
    - 환경 변수를 코드에서 분리하여 관리
    - @PostConstruct 사용을 위해 javax.annotation-api:1.3.2 라이브러리 추가

--- src/main/java/org/scoula/security/util/JwtProcessor.java
+++ src/main/java/org/scoula/security/util/JwtProcessor.java
@@ -4,18 +4,41 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
+@PropertySource({"classpath:/application.properties"})
 @Component
 public class JwtProcessor { // 헬퍼클래스란?
-    static private final long TOKEN_VALID_MILISECOND = 1000L * 60 * 10; // 1
-    private String secretKey = "충분히 긴 임의의(랜덤한) 비밀키 문자열 배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf ";
-    private Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+//    static private final long TOKEN_VALID_MILISECOND = 1000L * 60 * 10; // 1
+
+    @Value("${jwt.token-validity}")
+    private int TOKEN_VALID_MILISECOND;
+//    private String secretKey = "zzzzzzzzzzzzzzzzz충분히 긴 임의의(랜덤한) 비밀키 문자열 배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf배정asdfasdasdasdf ";
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    private Key key;
+//    private Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 // private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); -- 운영시 사용
+
+    @Value("${jdbc.testNumber}")
+    int testNumber;
+
+    @PostConstruct
+    public void init() {
+        // secretKey가 주입된 이후에 key 값을 초기화
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        System.out.println("TOKEN_VALID_MILISECOND" + TOKEN_VALID_MILISECOND);
+        System.out.println("secretKey" + secretKey);
+    }
 
     // JWT 생성
     public String generateToken(String subject) {
@@ -28,8 +51,8 @@ public class JwtProcessor { // 헬퍼클래스란?
     }
 
     // JWT Subject(username) 추출 - 해석 불가인 경우 예외 발생
-// 예외 ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException,
-// IllegalArgumentException
+    // 예외 ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException,
+    // IllegalArgumentException
     public String getUsername(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
